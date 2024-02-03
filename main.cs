f0fb9ac180fcd2a341f271efb52291b8ebb41f13
andrejamesfranc <31803608-andrejamesfranc@users.noreply.replit.com>
2024-02-03 02:50:22 +0000

    Lastname and Age

--- main.cs
+++ main.cs
@@ -2,8 +2,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    string firstname = "Andre";
-    Console.WriteLine ($"Hello World {firstname}");
-    Console.WriteLine ("Hello World" + firstname);
+    string Firstname = "Andre";
+    string Lastname = "Francisco";
+    int Age = 20;
+    Console.WriteLine ($"Hello World {Firstname} {Lastname} {Age}");
+    Console.WriteLine ("Hello World" + " " + Firstname + " " + Lastname + " " + Age);
   }
 }
