f08aeafa958c5a9b762c758962a7e6728a5c14a3
andrejamesfranc <31803608-andrejamesfranc@users.noreply.replit.com>
2024-02-03 02:45:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstname = "Andre";
+    Console.WriteLine ($"Hello World {firstname}");
+    Console.WriteLine ("Hello World" + firstname);
+  }
+}
