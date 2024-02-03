7c6f63827787e605c5e20c1b8d9a82eaa5ba2226
Yakitori31 <158657248+Yakitori31@users.noreply.github.com>
2024-02-03 03:26:30 +0000

    Course

--- main.cs
+++ main.cs
@@ -2,11 +2,12 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    string Firstname = "Andre";
+    string Firstname = "Andre James";
     string Lastname = "Francisco";
     int Age = 20;
     string Address = "Mabolo, City of Malolos, Bulacan";
-    Console.WriteLine ($"Hello World {Firstname} {Lastname} {Age} {Address}");
-    Console.WriteLine ("Hello World" + " " + Firstname + " " + Lastname + " " + Age + " " + Address);;
+    string Course = "Bachelor of Science in Information Technology";
+    Console.WriteLine ($"Hello World {Firstname} {Lastname} {Age} {Address} {Course}");
+    Console.WriteLine ("Hello World" + " I am " + Firstname + " " + Lastname + " My age is " + Age + " My Address is " + Address + " My Course in college is " + Course);
   }
 }
